38e73ab339e156c2ac1eb6f55961ee81004ed6a8
Bernard Maltais <bmaltais@gmail.com>
2020-07-02 13:38:15 +0000

    Moving to for_each

--- policy-Diagnostic-AA.tf
+++ policy-Diagnostic-AA.tf
@@ -1,12 +1,24 @@
 # https://github.com/Azure/Enterprise-Scale/blob/main/docs/reference/contoso/e2e-landing-zone-vwan-orchestration.parameters.json
 
+locals {
+  policies = {
+    Deploy-Diagnostics-AA = {
+      description = "Apply diagnostic settings for Azure Automation Accounts - Log Analytics"
+    },
+    Deploy-Diagnostics-ActivityLog = {
+      description = "Ensures that Activity Log Diagnostics settings are set to push logs into Log Analytics"
+    }
+  }
+}
+
 resource "azurerm_policy_definition" "Deploy-Diagnostics-AA" {
-  # count        = var.deployOptionalFeatures.deny_publicips_on_nics ? 1 : 0
-  name         = "Deploy-Diagnostics-AA"
+  for_each = local.policies
+
+  name         = each.key
   policy_type  = "Custom"
   mode         = "All"
-  display_name = "Deploy-Diagnostics-AA"
-  description  = "Apply diagnostic settings for Azure Automation Accounts - Log Analytics"
+  display_name = each.key
+  description  = each.value.description
   parameters   = file("${path.module}/policies/Deploy-Diagnostics-parameters.json")
-  policy_rule  = file("${path.module}/policies/Deploy-Diagnostics-AA.json")
+  policy_rule  = file("${path.module}/policies/${each.key}.json")
 }
