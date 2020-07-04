ead226ceb8dc2314e9a17364325302d5bbc9adf6
Bernard Maltais <bmaltais@gmail.com>
2020-07-04 15:39:58 +0000

    test

--- module.tf
+++ module.tf
@@ -73,7 +73,10 @@ PARAMETERS
                     "value": "[parameters('prefix')]"
                 }
             },
-            "policyDefinitionId": "[concat('/subscriptions/', subscription().subscriptionId, '/providers/Microsoft.Authorization/policyDefinitions/Deploy-Diagnostics-AA')]"
+            "variables": {
+                "policyDefinitionId": "/subscriptions/ed433324-b886-4c85-b62c-49283b83cafd/providers/Microsoft.Authorization/policyDefinitions/Deploy-Diagnostics-AA"
+            },
+            "policyDefinitionId": "[variables('policyDefinitionId')]"
         },
         {
             "parameters": {
