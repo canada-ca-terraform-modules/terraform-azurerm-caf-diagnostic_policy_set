1ab9ea1d33882a7bf72fd0c9737f46ea6907f9ef
Bernard Maltais <bmaltais@gmail.com>
2020-07-04 14:39:14 +0000

    Update

--- module.tf
+++ module.tf
@@ -62,7 +62,7 @@ resource "azurerm_policy_set_definition" "policy_set_definition" {
                     "value": "${var.log_analytics_workspace.name}-"
                 }
             },
-            "policyDefinitionId": "[concat('/subscriptions/', field('subscriptionId'), '/providers/Microsoft.Authorization/policyDefinitions/', 'Deploy-Diagnostics-AA')]"
+            "policyDefinitionId": "[concat('/subscriptions/', field('Microsoft.Subscription/SubscriptionDefinitions/subscriptionId'), '/providers/Microsoft.Authorization/policyDefinitions/', 'Deploy-Diagnostics-AA')]"
         },
         {
             "parameters": {
