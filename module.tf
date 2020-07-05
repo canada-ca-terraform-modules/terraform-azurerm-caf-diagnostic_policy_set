78990d82e797e01f21c17b50eb298ccf76e810a2
Bernard Maltais <bmaltais@gmail.com>
2020-07-05 01:39:12 +0000

    Update

--- module.tf
+++ module.tf
@@ -4,45 +4,45 @@ locals {
   policy_set_name = substr("${var.env}-${var.userDefinedString} diagnostic policy set", 0, 64)
   policies = {
     AA = {
-      name = "Deploy-Diagnostics-AA"
+      name        = "Deploy-Diagnostics-AA"
       description = "Apply diagnostic settings for Azure Automation Accounts - Log Analytics"
     },
     ActivityLog = {
-      name = "Deploy-Diagnostics-ActivityLog"
+      name        = "Deploy-Diagnostics-ActivityLog"
       description = "Ensures that Activity Log Diagnostics settings are set to push logs into Log Analytics"
     },
     KeyVault = {
-      name = "Deploy-Diagnostics-KeyVault"
+      name        = "Deploy-Diagnostics-KeyVault"
       description = "Apply diagnostic settings for Azure KeyVault - Log Analytics"
     },
     NIC = {
-      name = "Deploy-Diagnostics-NIC"
+      name        = "Deploy-Diagnostics-NIC"
       description = "Apply diagnostic settings for Azure NIC - Log Analytics"
     },
     NSG = {
-      name = "Deploy-Diagnostics-NSG"
+      name        = "Deploy-Diagnostics-NSG"
       description = "Apply diagnostic settings for Azure NSG - Log Analytics"
     },
     Recovery_Vault = {
-      name = "Deploy-Diagnostics-Recovery_Vault"
+      name        = "Deploy-Diagnostics-Recovery_Vault"
       description = "Apply diagnostic settings for Azure Recovery Vault - Log Analytics"
     },
     VM = {
-      name = "Deploy-Diagnostics-VM"
+      name        = "Deploy-Diagnostics-VM"
       description = "Apply diagnostic settings for Azure VM - Log Analytics"
     },
     VMSS = {
-      name = "Deploy-Diagnostics-VMSS"
+      name        = "Deploy-Diagnostics-VMSS"
       description = "Apply diagnostic settings for Azure VM Scale Set - Log Analytics"
     },
     VNET = {
-      name = "Deploy-Diagnostics-VNET"
+      name        = "Deploy-Diagnostics-VNET"
       description = "Apply diagnostic settings for Azure VNET - Log Analytics"
     }
   }
   subscriptionID = data.azurerm_subscription.primary.subscription_id
-  policySet = [
-    for item in local.policies :
+  policy_assignment = [
+    for policy in local.policies :
     {
       "parameters" : {
         "logAnalytics" : {
@@ -52,12 +52,12 @@ locals {
           "value" : "[parameters('prefix')]"
         }
       },
-      "policyDefinitionId" : "/subscriptions/${local.subscriptionID}/providers/Microsoft.Authorization/policyDefinitions/${item.name}"
+      "policyDefinitionId" : "/subscriptions/${local.subscriptionID}/providers/Microsoft.Authorization/policyDefinitions/${policy.name}"
     }
   ]
 }
 
-resource "azurerm_policy_definition" "Deploy-Diagnostics" {
+resource "azurerm_policy_definition" "policy_definition" {
   for_each = var.deploy ? local.policies : {}
 
   name         = each.value.name
@@ -68,125 +68,15 @@ resource "azurerm_policy_definition" "Deploy-Diagnostics" {
   parameters   = file("${path.module}/policies/Deploy-Diagnostics-parameters.json")
   policy_rule  = file("${path.module}/policies/${each.value.name}.json")
 }
-/*
-data "template_file" "policy_definitions" {
-  template = file("${path.module}/policies/Deploy-Diagnostics-policySetDefinition.json")
-  vars = {
-    subscriptionID = data.azurerm_subscription.primary.subscription_id
-  }
-*/
+
 resource "azurerm_policy_set_definition" "policy_set_definition" {
+  depends_on         = [resource.azurerm_policy_definition.policy_definition]
   count              = var.deploy ? 1 : 0
   name               = local.policy_set_name
   policy_type        = "Custom"
   display_name       = local.policy_set_name
   parameters         = file("${path.module}/policies/Deploy-Diagnostics-parameters.json")
-  policy_definitions = jsonencode(local.policySet) # data.template_file.policy_definitions.rendered
-  /*
-  <<POLICY_DEFINITIONS
-    [
-        {
-            "parameters": {
-                "logAnalytics": {
-                    "value": "[parameters('logAnalytics')]"
-                },
-                "prefix": {
-                    "value": "[parameters('prefix')]"
-                }
-            },
-            "policyDefinitionId": "${azurerm_policy_definition.Deploy-Diagnostics["Deploy-Diagnostics-AA"].id}"
-        },
-        {
-            "parameters": {
-                "logAnalytics": {
-                    "value": "[parameters('logAnalytics')]"
-                },
-                "prefix": {
-                    "value": "[parameters('prefix')]"
-                }
-            },
-            "policyDefinitionId": "${azurerm_policy_definition.Deploy-Diagnostics["Deploy-Diagnostics-ActivityLog"].id}"
-        },
-        {
-            "parameters": {
-                "logAnalytics": {
-                    "value": "[parameters('logAnalytics')]"
-                },
-                "prefix": {
-                    "value": "[parameters('prefix')]"
-                }
-            },
-            "policyDefinitionId": "${azurerm_policy_definition.Deploy-Diagnostics["Deploy-Diagnostics-KeyVault"].id}"
-        },
-        {
-            "parameters": {
-                "logAnalytics": {
-                    "value": "[parameters('logAnalytics')]"
-                },
-                "prefix": {
-                    "value": "[parameters('prefix')]"
-                }
-            },
-            "policyDefinitionId": "${azurerm_policy_definition.Deploy-Diagnostics["Deploy-Diagnostics-NIC"].id}"
-        },
-        {
-            "parameters": {
-                "logAnalytics": {
-                    "value": "[parameters('logAnalytics')]"
-                },
-                "prefix": {
-                    "value": "[parameters('prefix')]"
-                }
-            },
-            "policyDefinitionId": "${azurerm_policy_definition.Deploy-Diagnostics["Deploy-Diagnostics-NSG"].id}"
-        },
-        {
-            "parameters": {
-                "logAnalytics": {
-                    "value": "[parameters('logAnalytics')]"
-                },
-                "prefix": {
-                    "value": "[parameters('prefix')]"
-                }
-            },
-            "policyDefinitionId": "${azurerm_policy_definition.Deploy-Diagnostics["Deploy-Diagnostics-Recovery_Vault"].id}"
-        },
-        {
-            "parameters": {
-                "logAnalytics": {
-                    "value": "[parameters('logAnalytics')]"
-                },
-                "prefix": {
-                    "value": "[parameters('prefix')]"
-                }
-            },
-            "policyDefinitionId": "${azurerm_policy_definition.Deploy-Diagnostics["Deploy-Diagnostics-VNET"].id}"
-        },
-        {
-            "parameters": {
-                "logAnalytics": {
-                    "value": "[parameters('logAnalytics')]"
-                },
-                "prefix": {
-                    "value": "[parameters('prefix')]"
-                }
-            },
-            "policyDefinitionId": "${azurerm_policy_definition.Deploy-Diagnostics["Deploy-Diagnostics-VM"].id}"
-        },
-        {
-            "parameters": {
-                "logAnalytics": {
-                    "value": "[parameters('logAnalytics')]"
-                },
-                "prefix": {
-                    "value": "[parameters('prefix')]"
-                }
-            },
-            "policyDefinitionId": "${azurerm_policy_definition.Deploy-Diagnostics["Deploy-Diagnostics-VMSS"].id}"
-        }
-    ]
-POLICY_DEFINITIONS
-*/
+  policy_definitions = jsonencode(local.policy_assignment)
 }
 
 resource "azurerm_policy_assignment" "policy_assignment" {
