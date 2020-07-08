8212aa68e2b2b9c6c40cd72b652d5acdab3a834e
Bernard Maltais <bmaltais@gmail.com>
2020-07-08 00:31:41 +0000

    Update

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "policy_set_definition" {
-  value = var.deploy ? azurerm_policy_set_definition.policy_set_definition : null
+  value = var.deploy ? azurerm_policy_set_definition.policy_set_definition[0] : null
 }
