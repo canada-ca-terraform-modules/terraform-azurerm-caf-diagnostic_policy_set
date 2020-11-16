5be53f01d114c95d230e7102160d62da8f7ac5aa
Bernard Maltais <bernard.maltais@canada.ca>
2020-11-16 12:54:10 +0000

    Addind CI/CD and auto documentation generation

--- variables.tf
+++ variables.tf
@@ -1,24 +1,26 @@
 variable "env" {
-  description = "Env"
+  description = "You can use a prefix to add to the list of resource groups you want to create"
+  type        = string
+}
+
+variable "userDefinedString" {
+  description = "UserDefinedString part of the name of the resource"
   type        = string
 }
 
 variable "log_analytics_workspace" {
   description = "Log analytics workspace object"
-}
-
-variable "userDefinedString" {
-  description = "User defined string for the module"
-  type        = string
+  type        = any
 }
 
 variable "management_group_name" {
-  description = "(Optional) The name of the Management Group where this policy should be defined. Changing this forces a new resource to be created."
+  description = "The name of the Management Group where this policy should be defined. Changing this forces a new resource to be created."
   type        = string
   default     = null
 }
 
 variable "policy_name_postfix" {
-  type    = string
-  default = ""
+  description = "The postfix value to append at the end of the generated resource name"
+  type        = string
+  default     = ""
 }
