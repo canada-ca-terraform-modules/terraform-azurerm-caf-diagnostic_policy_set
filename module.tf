1152617f104c116f5e43f28101a479649c7faa90
Bernard Maltais <bernard.maltais@canada.ca>
2021-06-02 08:17:58 -0400

    Update

--- module.tf
+++ module.tf
@@ -36,11 +36,11 @@ resource "azurerm_policy_set_definition" "policy_set_definition" {
   display_name          = "${local.policy_set_name}${var.policy_name_postfix}"
   description           = "This initiative configures application Azure resources to forward diagnostic logs and metrics to an Azure Log Analytics workspace."
   management_group_name = var.management_group_name == null ? null : var.management_group_name
-  parameter_values      = file("${path.module}/policies/Deploy-Diagnostics-parameters.json")
+  parameters            = file("${path.module}/policies/Deploy-Diagnostics-parameters.json")
   dynamic "policy_definition_reference" {
     for_each = jsondecode(local.policies_json)
     content {
-      parameters = {
+      parameter_values = {
         "logAnalytics" = "[parameters('logAnalytics')]"
         "prefix"       = "[parameters('prefix')]"
       }
